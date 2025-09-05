5a1ca3de505aa5b2cd47b42f18c664a8b2f6f0a3
jmarcell <jmarcell@student.42madrid.com>
2025-09-05 14:11:49 +0200

    echo terminado

--- src/exec/builtins/echo.c
+++ src/exec/builtins/echo.c
@@ -12,9 +12,50 @@
 
 #include "exec_builtins.h"
 
+static int	check_flag(char *str)
+{
+	int	i;
+
+	i = 1;
+	while (str[i])
+	{
+		if (str[i] == 'n')
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
 int	msh_exec_bt_echo(t_ent *cmd)
 {
-	if (cmd)
-		cmd = NULL;
+	int	i;
+	int	newline;
+	int	check;
+
+	i = 1;
+	newline = 1;
+	check = 1;
+	while (cmd->argv[i] && !ft_strncmp(cmd->argv[i], "-n", 2) && check)
+	{
+		check = check_flag(cmd->argv[i]);
+		if (i == 1 && check)
+			newline = 0;
+		i++;
+	}
+	i--;
+	while (cmd->argv[i])
+    {
+        printf("%s", cmd->argv[i]);
+        if (cmd->argv[i + 1])
+            printf(" ");
+        i++;
+    }
+	if (newline)
+		printf("\n");
 	return (0);
+	
+	// if (cmd)
+	// 	cmd = NULL;
+	// return (0);
 }
