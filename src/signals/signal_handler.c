d78c464c935b4e40885325a67881b327e5b882e4
marcnava <marcnava@student.42madrid.com>
2025-09-25 04:24:35 +0200

    hotfix

--- src/signals/signal_handler.c
+++ src/signals/signal_handler.c
@@ -6,11 +6,12 @@
 /*   By: marcnava <marcnava@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 20:19:10 by jmarcell          #+#    #+#             */
-/*   Updated: 2025/09/21 00:46:41 by marcnava         ###   ########.fr       */
+/*   Updated: 2025/09/25 04:22:07 by marcnava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <string.h>
 
 // Variable global para almacenar el exit_code cuando se recibe una señal
 volatile sig_atomic_t	g_signal_received = 0;
@@ -62,14 +63,14 @@ void	setup_parent_signals(void)
 	struct sigaction	sa_int;
 	struct sigaction	sa_quit;
 
-	sa_int.sa_handler = handle_sigint;
-	sigemptyset(&sa_int.sa_mask);
-	sa_int.sa_flags = SA_RESTART;
-	sigaction(SIGINT, &sa_int, NULL);
-	sa_quit.sa_handler = SIG_IGN;
-	sigemptyset(&sa_quit.sa_mask);
-	sa_quit.sa_flags = 0;
-	sigaction(SIGQUIT, &sa_quit, NULL);
+    sa_int.sa_handler = handle_sigint;
+    memset(&sa_int.sa_mask, 0, sizeof(sa_int.sa_mask));
+    sa_int.sa_flags = SA_RESTART;
+    sigaction(SIGINT, &sa_int, NULL);
+    sa_quit.sa_handler = SIG_IGN;
+    memset(&sa_quit.sa_mask, 0, sizeof(sa_quit.sa_mask));
+    sa_quit.sa_flags = 0;
+    sigaction(SIGQUIT, &sa_quit, NULL);
 }
 
 /**
